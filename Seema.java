7dd928b2ef472948c5db35e57c51108c11c7d329
SeemaAggarwal <121787754+DkAggarwal2512@users.noreply.github.com>
2024-06-06 15:10:53 +0530

    Add files via upload

---
+++ Seema.java
@@ -0,0 +1,14 @@
+package demo.java;
+
+public class Seema {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My First Eclips Programe.");
+		
+
+	}
+	
+}
+
+
